bd9916a7ff7b21155b118453ba2629be98d268b7
Abdul Hadi Istiyak <abdulhadiistiyak@gmail.com>
2026-02-28 13:59:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "aether-assistant-4rs4r",
+  "appId": "1:633901286866:web:171a02459b24bb6559f40b",
+  "apiKey": "AIzaSyBaoDKNmN8TgWrwuhsfk-PEBe1kDx2Hk2o",
+  "authDomain": "aether-assistant-4rs4r.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "633901286866"
+};
